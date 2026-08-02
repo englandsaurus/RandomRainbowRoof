7b5054afca6a23e736d453757283858d0210735f
englandsaurus <57819547+englandsaurus@users.noreply.github.com>
2026-08-02 09:27:19 -0600

    [feat] add audio

--- TouchDesigner/RandomRainbowRoof/Components/pongEXT.py
+++ TouchDesigner/RandomRainbowRoof/Components/pongEXT.py
@@ -1,9 +1,24 @@
+import threading
+import time
+
 class Pong:
 	def __init__(self, ownerComp):
 		self.ownerComp = ownerComp
+		self.bullet = ownerComp.op('bsolver1')
+		self.start_trigger = ownerComp.op('start_sim')
+		self.ball = ownerComp.op('ball')
+		self.winner = ownerComp.op('Win_animation/winner')
+		print(f'{__class__.__name__} class initialized from {self.ownerComp.name}.')
+		pass
 	
-	def SwitchPage(self, current_page, next_page):
-		current_page.par.display = 0
-		next_page.allowCooking = True
-		next_page.par.display = 1
-		current_page.allowCooking = False
+	def Score_point(self, Channel):
+		if Channel.index == 1:
+			self.ball.par.linvelx = -10
+		else:
+			self.ball.par.linvelx = 10
+		self.bullet.par.initall.pulse()
+		self.start_trigger.par.trigger.pulse()
+		pass
+	
+	def Win_game(self, Channel):
+		self.winner[0].val = Channel
